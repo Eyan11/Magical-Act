e0e710500c955f28124ee6aebbb5d6cd35d76620
Eyan <eyanmartucci11@gmail.com>
2024-06-12 18:45:41 -0700

    Adjusted Player Movement and Colliders
    - Magician and Rabbit now have 2 colliders; a box collider for normal collision, and a circle collider on feet to prevent getting stuck between blocks.
    - Updated movement code to use AddForce() instead of setting velocity.
    - Updated IsGrounded so that it also requires velocity.y < 0.1 to prevent jumping while still moving upwards
    - Added jump input buffer so that jump can be pressed slightly before landing and player will still jump

--- Mini Jame Gam #31/Assets/Scripts/Player/PlayerMovement.cs
+++ Mini Jame Gam #31/Assets/Scripts/Player/PlayerMovement.cs
@@ -14,15 +14,18 @@ public class PlayerMovement : MonoBehaviour
     [Header ("Settings")]
     [SerializeField] private float groundedRayInterval;
     [SerializeField] private float coyoteTime;
-    [SerializeField] private float moveAcceleration;
-    [SerializeField] private float moveDeaccelerationFactor;
-    [SerializeField] private float moveMaxSpeed;
-    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float jumpBufferTime;
+    [SerializeField] private float maxMoveSpeed;
+    [SerializeField] private float moveForce;
+    [SerializeField] private float jumpForce;
     [SerializeField] private float groundedRayDist;
-
+    [SerializeField] private float groundedDrag;
+    [SerializeField] private float airDrag;
+    private const int MOVE_FORCE_MULT = 50;
     private float moveDir;
-    private Vector2 curVelocity;
+    private Vector2 curMoveForce;
     private float canJumpTimer;
+    private float jumpInputTimer = 0f;
     public bool IsGrounded { get; private set;}
     private Vector3 playerScale = Vector3.one;
 
@@ -34,7 +37,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void OnEnable() {
-        curVelocity = Vector2.zero;
+        curMoveForce = Vector2.zero;
         playerScale = Vector3.one;
         canJumpTimer = -1f;
         StartCoroutine(GroundCheckCoroutine());
@@ -46,37 +49,25 @@ public class PlayerMovement : MonoBehaviour
     }
     
     private void Update() {
-        canJumpTimer -= Time.deltaTime;
-        MovementCalculations();
+        MoveCalculations();
+        JumpCalculations();
         LimitSpeed();
-        MovePlayer();
+    }
+
+    private void FixedUpdate() {
+        body.AddForce(curMoveForce * MOVE_FORCE_MULT, ForceMode2D.Force);
     }
 
     /** Calculates x and y direction movement **/
-    private void MovementCalculations() {
-
+    private void MoveCalculations() {
+        // input
         moveDir = inputScript.MoveInput;
 
-        // if no input, slow down
-        if(moveDir == 0f && curVelocity.x != 0f) {
-            curVelocity.x *= moveDeaccelerationFactor * Time.deltaTime;
-            
-            // set velocity to 0
-            if(Mathf.Abs(curVelocity.x) < 0.2f)
-                curVelocity.x = 0f;
-        }
-        // if input, speed up
+        // move force
+        if(moveDir == 0f)
+            curMoveForce.x = 0f;
         else
-            curVelocity.x += moveDir * moveAcceleration * Time.deltaTime;
-
-        if(inputScript.JumpInput && canJumpTimer > 0f)
-            Jump();
-    }
-
-    /** Applied movement calculations to rigidbody and rotates player **/
-    private void MovePlayer() {
-        curVelocity.y = body.velocity.y;
-        body.velocity = curVelocity;
+            curMoveForce.x = moveDir * moveForce;
 
         // make player face move input
         if(inputScript.MoveInput != 0f)
@@ -84,19 +75,35 @@ public class PlayerMovement : MonoBehaviour
         transform.localScale = playerScale;
     }
 
+    private void JumpCalculations() {
+        canJumpTimer -= Time.deltaTime;
+        jumpInputTimer -= Time.deltaTime;
+
+        // jump input buffer
+        if(inputScript.JumpInput)
+            jumpInputTimer = jumpBufferTime;
+
+        if(jumpInputTimer > 0f && canJumpTimer > 0f)
+            Jump();
+    }
+
     /** Caps x velocity at moveSpeed **/
     private void LimitSpeed() {
 
-        if(curVelocity.x > moveMaxSpeed)
-            curVelocity.x = moveMaxSpeed;
-        else if(curVelocity.x < -moveMaxSpeed)
-            curVelocity.x = -moveMaxSpeed;
+        if(body.velocity.x > maxMoveSpeed)
+            body.velocity = new Vector2(maxMoveSpeed, body.velocity.y);
+        else if(body.velocity.x < -maxMoveSpeed)
+            body.velocity = new Vector2(-maxMoveSpeed, body.velocity.y);
+
+        Debug.Log(body.velocity.x);
     }
 
     /** Applies jump speed to rigidbody **/
     private void Jump() {
         canJumpTimer = -1f;
-        body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        jumpInputTimer = -1f;
+        body.drag = airDrag;
+        body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
     /** Returns true if grounded **/
@@ -105,9 +112,12 @@ public class PlayerMovement : MonoBehaviour
         IsGrounded = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, 
             Vector2.down, groundedRayDist, floorLayer);
 
-        if(IsGrounded == true) {
+        if(IsGrounded == true && body.velocity.y < 0.1) {
             canJumpTimer = coyoteTime;
+            body.drag = groundedDrag;
         }
+        else
+            body.drag = airDrag;
 
         yield return new WaitForSeconds(groundedRayInterval);
         StartCoroutine(GroundCheckCoroutine());
